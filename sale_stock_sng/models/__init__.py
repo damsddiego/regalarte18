d78c06bee21afc7c4f22170f633c5b068970018d
Michael Silva <michael.silva@manexware.com>
2025-11-17 15:08:21 -0500

    Al generar una transferencia este muestre como bodega de origen la bodega del vendedor y la bodega de destino aparezca la bodega del cliente

--- sale_stock_sng/models/__init__.py
+++ sale_stock_sng/models/__init__.py
@@ -2,3 +2,4 @@ from . import res_partner
 from . import sale_order
 from . import stock_rule
 from . import stock_quant
+from . import stock_picking
---
+++ sale_stock_sng/models/stock_picking.py
@@ -0,0 +1,59 @@
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.onchange('partner_id')
+    def _onchange_partner_sale_locations(self):
+        """
+        Al seleccionar el contacto en el picking:
+        - Solo aplica si el tipo de picking es transferencia entre bodegas.
+        - La bodega de origen (`location_id`) se toma de la bodega del vendedor
+          (res.users -> res.partner -> sale_location_id).
+        - La bodega de destino (`location_dest_id`) se toma del contacto seleccionado
+          (partner_id.sale_location_id).
+        """
+        for picking in self:
+
+            if picking.picking_type_id.code != 'internal':
+                continue
+
+            if not picking.partner_id:
+                continue
+
+            user_sale_location = None
+            if picking.partner_id.user_id:
+                user_sale_location = picking.partner_id.user_id.partner_id.sale_location_id
+
+            partner_sale_location = picking.partner_id.sale_location_id
+
+            if user_sale_location:
+                picking.location_id = user_sale_location
+
+            if partner_sale_location:
+                picking.location_dest_id = partner_sale_location
+
+    # @api.model
+    # def create(self, vals):
+    #     picking = super().create(vals)
+    #     if picking.picking_type_id.code == 'internal':
+    #         picking._set_default_sale_locations()
+    #     return picking
+    #
+    # def _set_default_sale_locations(self):
+    #     for picking in self:
+    #         if picking.picking_type_id.code != 'internal':
+    #             continue
+    #
+    #         user_sale_location = None
+    #         if picking.partner_id.user_id:
+    #             user_sale_location = picking.partner_id.user_id.partner_id.sale_location_id
+    #
+    #         partner_sale_location = picking.partner_id.sale_location_id
+    #
+    #         if user_sale_location and not picking.location_id:
+    #             picking.location_id = user_sale_location
+    #
+    #         if partner_sale_location and not picking.location_dest_id:
+    #             picking.location_dest_id = partner_sale_location
